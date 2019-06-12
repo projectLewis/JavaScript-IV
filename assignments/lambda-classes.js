d39b69555ff48131e720b5a3062ffecf8d099a91
Seth Lewis <lewiscatenary@gmail.com>
2019-06-12 17:33:17 -0500

    added test personnel for each class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -22,13 +22,18 @@ class Instructors extends Person {
   grade(student, subject) {
     return `${ student.name } receives a perfect score on ${ subject }`;
   }
+  adjustGrade(student) {
+    student.grade = Math.floor((Math.random() * 100) + 1);
+    return student.grade;
+  }
 }
 class Students extends Person {
-  constructor(sName, sAge, sLocal, previousBackground, className, favSubjects) {
+  constructor(sName, sAge, sLocal, previousBackground, className, favSubjects, grade) {
     super(sName, sAge, sLocal);
     this.previousBackground = previousBackground;
     this.className = className;
     this.favSubjects = favSubjects;
+    this.grade = grade;
   }
   listSubjects() {
     this.favSubjects.forEach((el) => {
@@ -36,10 +41,18 @@ class Students extends Person {
     });
   }
   PRAssignment(subject) {
-    return `${ student.name } has submitted a PR for ${ subject }`;
+    return `${ this.name } has submitted a PR for ${ subject }`;
   }
   sprintChallenge(subject) {
-    return `${ student.name } has begun sprint challenge on ${ subject }`;
+    return `${ this.name } has begun sprint challenge on ${ subject }`;
+  }
+  graduate() {
+    if (this.grade >= 70) {
+      return `CONGRATULATIONS!!! ${ this.name } you are eligible for graduation!`;
+    }
+    else {
+      return `Sorry, ${ this.name } not quite ready for graduation yet. Keep studying and pull those grades up.`;
+    }
   }
 }
 class ProjectManagers extends Instructors {
@@ -55,3 +68,87 @@ class ProjectManagers extends Instructors {
     return `${ this.name } debugs ${ student.name }'s code on ${ subject }`;
   }
 }
+
+/// Test Personnel
+
+const bigMike = new Instructors(
+  "Big Mike",
+  30,
+  "new guinea",
+  "Go Lang",
+  "Rust",
+  "Wait this is a Web Dev course??"
+)
+const lilMike = new Instructors(
+  "Lil Mike",
+  15,
+  "Egypt",
+  "backend",
+  "C#",
+  "Yes, I am a child prodigy..."
+)
+const mike = new Instructors(
+  "Mike",
+  42,
+  "New Mexico",
+  "frontend",
+  "Javascript",
+  "No I'm the real Mike!!"
+)
+const bigWill = new Students(
+  "Big Will",
+  22,
+  "Scotland",
+  "tv star",
+  "web27",
+  "CSS",
+  68
+)
+const lilWill = new Students(
+  "Lil Will",
+  28,
+  "France",
+  "tv star",
+  "web27",
+  "HTML",
+  85
+)
+const will = new Students(
+  "Will",
+  24,
+  "Spain",
+  "TV star",
+  "web27",
+  "Javascript",
+  97
+)
+const shakur = new ProjectManagers(
+  "Mr. Shakur",
+  "24",
+  "West Coast",
+  "machine learning",
+  "Python",
+  "PM LIFE!",
+  "Web0",
+  "Big Mike"
+)
+const wallace = new ProjectManagers(
+  "Mr. Shakur",
+  "23",
+  "Brooklyn",
+  "web development",
+  "Typescript",
+  "Where Brooklyn at!?",
+  "Web0",
+  "Big Mike"
+)
+const rios = new ProjectManagers(
+  "Mr. Rios",
+  "27",
+  "The Bronx",
+  "Node",
+  "Javscript",
+  "I'm not a PM, I just grade a lot.",
+  "Web0",
+  "Big Mike"
+)
