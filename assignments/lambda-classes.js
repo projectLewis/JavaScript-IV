0d781bda7c658b2e610d44dad36413b80f1d00fa
Seth Lewis <lewiscatenary@gmail.com>
2019-06-12 16:43:23 -0500

    initial structure

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,25 @@
-// CODE here for your Lambda Classes
+// See README for detailed instructions
+class Person {
+  constructor(name, age, location) {
+
+  }
+
+}
+class Instructors extends Person {
+  constructor(iName, iAge, iLocal, specialty, favLanguage, catchPhrase) {
+
+  }
+
+}
+class Students extends Person {
+  constructor(sName, sAge, sLocal, previousBackground, className, faveSubjects) {
+
+  }
+
+}
+class ProjectManagers extends Instructors {
+  constructor(pName, pAge, pLocal, pSpecialty, pFavLanguage, pCatchPhrase, gradClassName, favInstructor) {
+
+  }
+
+}
